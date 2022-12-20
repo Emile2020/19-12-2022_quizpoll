8baed4c04e036d5244092cfae0c99d4773cf8018
Emile2020 <emiledebruyckere@gmail.com>
2022-12-20 11:18:39 +0100

    Added admin page
    CONTAINS THE FOLLOWING ERRORS:
    - JSON.parse: unexpected end of data at line 1 column 1 of the JSON data
    - TypeError: document.getElementById(...) is null
    
    I have tried to fix the errors but I am not able to do so. Please help me out.
    
    DO NOT USE THE CODE FROM THIS BRANCH, IT IS INSECURE AND FULL OF ERRORS.

--- index.js
+++ index.js
@@ -2,21 +2,42 @@ import express from 'express';
 import path from 'path';
 import fs from 'fs'
 import bodyParser from 'body-parser';
+import nodemail from 'nodemailer';
+const __dirname = path.resolve();
+//get data from ./src/json/index.json
+var data = JSON.parse(fs.readFileSync(__dirname + '/src/json/index.json', 'utf8'));
+//init email
+var transporter = nodemail.createTransport({
+    service: 'gmail',
+    auth: {
+        user: data.email,
+        pass: data['app-password']
+    }
+});
+
 const app = express();
 app.use(bodyParser.json());
-const __dirname = path.resolve();
 function init() {
     //start the pages
     app.get('/', (req, res) => {
         res.sendFile(__dirname + '/src/pages/index.html');
     });
+    app.get('/admin', (req, res) => {
+        res.sendFile(__dirname + '/src/pages/admin.html');
+    });
     //start the css
     app.get('/index.css', (req, res) => {
         res.sendFile(__dirname + '/src/styles/index.css');
     });
+    app.get('/admin.css', (req, res) => {
+        res.sendFile(__dirname + '/src/styles/admin.css');
+    });
     //start the js
     app.get('/index.js', (req, res) => {
         res.sendFile(__dirname + '/src/scripts/index.js');
+    });
+    app.get('/admin.js', (req, res) => {
+        res.sendFile(__dirname + '/src/scripts/admin.js');
     });
     //start the json
     app.get('/index.json', (req, res) => {
@@ -60,6 +81,47 @@ function init() {
             //send the response
             res.send('invalid request');
             res.statusCode = 400;
+        }
+    })
+    app.post('/validatecookie', (req, res) => {
+        //check if the cookie is valid
+        if (data.valid_tokens.includes(req.body.token)) {
+            //send the response
+            res.send('valid');
+        } else {
+            //send the response
+            res.send('invalid');
+        }
+    })
+    app.post('/admin/api', (req, res) => {
+        if (req.body.password && req.body.email && req.body.newjson) {
+            if (req.body.password == data['app-password'] && req.body.email == data.email) {
+                fs.writeFile(__dirname + '/src/json/index.json', req.body.newjson, (err) => {
+                    if (err) {
+                        console.error(err)
+                        return
+                    }
+                    //send an email with the new json and old json
+                    if (data.email_send) {
+                        transporter.sendMail({
+                            from: data.email,
+                            to: data.email,
+                            subject: 'New json',
+                            text: 'New json: ' + req.body.newjson + '\nOld json: ' + data
+                        }, (err, info) => {
+                            if (err) {
+                                console.error(err)
+                                return
+                            }
+                            console.log(info)
+                        })
+                    }
+                    //send the response 
+                    res.send('success');
+                })
+            } else {
+                res.send('invalid credentials');
+            }
         }
     })
     //start the server
